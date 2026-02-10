9673952bec818802a2d7dd7c5239bfbca62f576f
BASSINAS <56053848+BASSINAS@users.noreply.github.com>
2026-02-11 00:09:50 +0100

    Create playwright.config.js

---
+++ playwright.config.js
@@ -0,0 +1,23 @@
+/** @type {import('@playwright/test').PlaywrightTestConfig} */
+const TestInfoReporter = require('./modules/testInfo');
+
+const config = {
+  testDir: './tests',
+  timeout: 30_000,
+  expect: {
+    timeout: 5000
+  },
+  use: {
+    headless: false,
+    viewport: { width: 1280, height: 720 },
+    actionTimeout: 10000,
+    ignoreHTTPSErrors: true
+  },
+  reporter: [
+    ['html'],
+    ['./modules/testInfo.js'],
+    ['./modules/jsonAppenderReporter2.js', { file: './test-results/aggregate-results.json' }]
+  ]
+};
+
+module.exports = config;
